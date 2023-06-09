fcb31abe398928acc4002e441b68d7c6fb7e4aed
Vanderson Nogueira <vanderson.01@hotmail.com>
2023-06-09 11:16:08 -0300

    Router created

--- src/main/java/br/com/example/HttpRouter/MainVerticle.java
+++ src/main/java/br/com/example/HttpRouter/MainVerticle.java
@@ -2,22 +2,99 @@ package br.com.example.HttpRouter;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
+	public static void main(String[] args) {
+		
+		 Vertx vertx = Vertx.vertx();
+	     vertx.deployVerticle(new MainVerticle());
+	}
+	
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
+
+   Router router = Router.router(vertx);
+
+// Rota para listagem de usuários
+    router.route("/api/usuarios").handler(ctx -> {
+      // Manipulação da requisição
+      ctx.response().putHeader("Content-Type", "application/json")
+       //sua lógica de negócio devolvendo uma listagem de usuários
+        .end(getUsers().encode());
     });
+
+// Rota para detalhes de um usuário específico
+    router.route("/api/usuarios/:id").handler(ctx -> {
+      String id = ctx.request().getParam("id");
+      // Manipulação da requisição
+      int statusCode = 200;
+      String message = "OK";
+      String body = "";
+      try {
+		
+    	  body =  findUser(Integer.parseInt(id)).encode();
+	} catch (Exception e) {
+		System.out.println(e);
+		statusCode = 404;
+		message = "User not found";
+	} 
+      ctx
+          .response()
+          .setStatusCode(statusCode)
+          .setStatusMessage(message)
+          .putHeader("Content-Type", "application/json")
+        .end(body);
+    });
+
+// Criação do servidor HTTP e configuração do roteador
+    vertx.createHttpServer()
+      .requestHandler(router)
+      .listen(8080 , http -> {
+          if (http.succeeded()) {
+              startPromise.complete();
+              System.out.println("Servidor HTTP Iniciado 8080");
+            } else {
+              startPromise.fail(http.cause());
+            }
+          });
   }
+
+    private JsonObject findUser(int id) {
+    	JsonArray users = createUsers();
+    	
+    	
+	return users.getJsonObject(id - 1);
+}
+
+	private JsonObject getUsers() {
+    	 // Create a JSON array for users
+        JsonArray users = createUsers();
+
+        // Create the response JSON object
+        JsonObject responseJson = new JsonObject();
+        responseJson.put("status", "200");
+        responseJson.put("message", "Listagem de usuários");
+        responseJson.put("users", users);
+	 return responseJson;
+}
+
+	private JsonArray createUsers() {
+		  JsonArray users = new JsonArray();
+        users.add(new JsonObject()
+        		.put("id", 1)
+                .put("name", "John Doe")
+                .put("age", 30));
+        users.add(new JsonObject()
+        		.put("id", 2)
+                .put("name", "Jane Smith")
+                .put("age", 35));
+        
+        return users;
+				
+	}
 }
---
+++ src/test/java/br/com/example/HttpRouter/MainVerticleTest.java
@@ -0,0 +1,83 @@
+package br.com.example.HttpRouter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientResponse;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+
+@ExtendWith(VertxExtension.class)
+public class MainVerticleTest {
+
+    private Vertx vertx;
+    private int port;
+
+    @BeforeEach
+    void setUp(Vertx vertx, VertxTestContext testContext) {
+        this.vertx = vertx;
+
+        // implanta the MainVerticle
+        vertx.deployVerticle(MainVerticle.class.getName(), testContext.succeeding(id -> testContext.completeNow()));
+
+        // Porta do server.
+        port = 8080;
+    }
+
+    @AfterEach
+    void tearDown(VertxTestContext testContext) {
+        vertx.close(testContext.succeeding(id -> testContext.completeNow()));
+    }
+
+    @Test
+    void testHelloVertxEndpoint(VertxTestContext testContext) {
+        // Create an HTTP client
+        HttpClient client = vertx.createHttpClient();
+
+        // Send a GET request to the Hello Vert.x endpoint
+        client.request(HttpMethod.GET, port, "localhost", "/api/usuarios")
+                .compose(req -> req.send().compose(HttpClientResponse::body))
+                .onSuccess(body -> {
+                	System.out.println(body);
+                    // Assert the response
+                    testContext.verify(() -> {
+                    	assertEquals("{\"status\":\"200\",\"message\":\"Listagem de usuários\",\"users\":[{\"name\":\"John Doe\",\"age\":30},{\"name\":\"Jane Smith\",\"age\":35}]}",
+                    			body.toString());
+                    });
+
+                    testContext.completeNow();
+                })
+                .onFailure(testContext::failNow);
+                
+    }
+    
+//    @Test
+//    void testHelloVertxEndpoint(VertxTestContext testContext) {
+//        // Create an HTTP client
+//        HttpClient client = vertx.createHttpClient();
+//
+//        // Send a GET request to the Hello Vert.x endpoint
+//        client.request(HttpMethod.GET, port, "localhost", "/api/usuarios")
+//                .compose(req -> req.send().compose(HttpClientResponse::body))
+//                .onSuccess(body -> {
+//                	System.out.println(body);
+//                    // Assert the response
+//                    testContext.verify(() -> {
+//                    	assertEquals("{\"status\":\"200\",\"message\":\"Listagem de usuários\",\"users\":[{\"name\":\"John Doe\",\"age\":30},{\"name\":\"Jane Smith\",\"age\":35}]}",
+//                    			body.toString());
+//                    });
+//
+//                    testContext.completeNow();
+//                })
+//                .onFailure(testContext::failNow);
+//                
+//    }
+}
+
